6a816748c517a2f589ae02cd22a83cbe95da21f1
Nastia Naydenko <stifutina@gmail.com>
2023-03-09 22:22:18 +0200

    fix deploy

--- server/api/data.ts
+++ server/api/data.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
 
-    console.log()
+    console.log(process.cwd())
     try { 
         fs.writeFileSync('./assets/data/data_v2.json', JSON.stringify(body), 'utf-8');
     } catch(e) {
