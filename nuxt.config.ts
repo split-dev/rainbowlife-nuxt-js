a5007a7f0f99b034dc4a4cedb336cd538cd468bb
Nastia Naydenko <stifutina@gmail.com>
2023-03-09 20:56:15 +0200

    fix deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     nitro: {
-        preset: 'node-server'
+        preset: 'vercel'
     },
 
     // @ts-expect-error: Type 'false' has no properties in common with type 'ViteConfig'
