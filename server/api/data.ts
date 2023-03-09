05ce1d89172f40ab2f4a3edd4dba0814cd0a48ab
Nastia Naydenko <stifutina@gmail.com>
2023-03-09 22:49:09 +0200

    fix deploy

--- server/api/data.ts
+++ server/api/data.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
     const IS_PROD = process.env.NODE_ENV === 'production';
-    const DATA_PATH = IS_PROD ? '../public/data/data_v2.json' : './public/data/data_v2.json';
+    const DATA_PATH = IS_PROD ? '../../public/data/data_v2.json' : './public/data/data_v2.json';
 
     try { 
         fs.writeFileSync(DATA_PATH, JSON.stringify(body), 'utf-8');
