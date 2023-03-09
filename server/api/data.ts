5a45dc052ff1e3e215ef3ac0fbcd3bcdaf2a34a5
Nastia Naydenko <stifutina@gmail.com>
2023-03-09 22:29:57 +0200

    fix deploy

--- server/api/data.ts
+++ server/api/data.ts
@@ -3,9 +3,11 @@ import fs from 'fs';
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
 
-    console.log('PATH', process.cwd())
+    console.log('PATH', process.cwd());
+    const pathParse = await fs.readdirSync(process.cwd(), { withFileTypes: true });
+    console.log('pathParse', pathParse);
     try { 
-        fs.writeFileSync('assets/data/data_v2.json', JSON.stringify(body), 'utf-8');
+        fs.writeFileSync('~/assets/data/data_v2.json', JSON.stringify(body), 'utf-8');
     } catch(e) {
         console.error(e);
 
