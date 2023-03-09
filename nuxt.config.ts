e0b5d5d343fc7e400e152597651873c38978dc1d
Nastia Naydenko <stifutina@gmail.com>
2023-03-09 21:39:38 +0200

    fix deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    nitro: {
-        preset: 'vercel'
-    },
+    vite: false,
     components: {
         global: true,
         dirs: ['~/components'],
