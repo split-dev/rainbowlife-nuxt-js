e3ac3a08f064f85404a6f5c410faedecdbd7dd68
Nastia Naydenko <stifutina@gmail.com>
2023-03-09 21:42:31 +0200

    fix deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    vite: false,
+    vite: {
+        ssr: {
+            // Add libraries containing invalid ESM here
+            noExternal: ['naive-ui']
+        }
+    },
     components: {
         global: true,
         dirs: ['~/components'],
